721b7f0c810bc7480919e47d7f9584199a01e53c
Zoltan David <david.zolka@gmail.com>
2019-09-02 19:44:10 +0200

    experimenting

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,6 +26,7 @@ const siteConfig = {
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
+    docsUrl: '',
     
     // Used for publishing and more
     projectName: 'configcat',
