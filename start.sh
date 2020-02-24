f53e1723b873abbf8ecf1a9343ddb599492a90b2
Lajos <lajos88.szoke@gmail.com>
2020-02-24 23:09:17 +0100

    remove html

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 envsubst '$$REDIRECT_TO' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
-rm -rf /usr/share/nginx/html
+rm -rf /usr/share/nginx/html/*
 mv /usr/share/nginx/update /usr/share/nginx/html
 nginx -g 'daemon off;'
