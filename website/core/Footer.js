b71bf753aa80b0bde1db452c20112d9f6a0c3df1
sige <sige@configcat.com>
2020-03-30 18:58:54 +0200

    footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -24,19 +24,28 @@ class Footer extends React.Component {
         <section className="sitemap">
           <a href={this.props.config.baseUrl} className="nav-home">
             {this.props.config.footerIcon && (
-              <img src={this.props.config.baseUrl + this.props.config.footerIcon} alt={this.props.config.title} width="66" height="58"/>
+              <img src={this.props.config.baseUrl + this.props.config.footerIcon} alt={this.props.config.title} width="66" height="58" />
             )}
           </a>
           <div>
-            <h5>Community</h5>
-            <a href="https://join.slack.com/t/configcat-support/shared_invite/enQtMzkwNDg3MjQ3MTUyLTY0ZGQxOWEwZmI0ZGJlOWY3YmRmOTRlNjcyOTY1Y2EzNTFlYzg1NTY0ZDAzNzU0YjkxMWVmNzVmOTE5MWUzOTI" target="_blank" >Project Chat on Slack</a>
+            <h5>Engage</h5>
             <a href="https://twitter.com/configcat" target="_blank" rel="noreferrer noopener">Twitter</a>
-                    <a href="https://www.facebook.com/configcat" target="_blank">Facebook</a>
-                    <a href="https://www.linkedin.com/company/configcat/" target="_blank">LinkedIn</a>
+            <a href="https://www.facebook.com/configcat" target="_blank">Facebook</a>
+            <a href="https://www.linkedin.com/company/configcat/" target="_blank">LinkedIn</a>
           </div>
           <div>
-            <h5>More</h5>
-            <a href="https://github.com/configcat" target="_blank">GitHub</a>
+            <h5>Know more</h5>
+            <a href="https://configcat.com" target="_blank">ConfigCat.com</a>
+            <a href="https://configcat.com/blog" target="_blank">Blog</a>
+            <a href="https://github.com/configcat" target="_blank">GitHub repos</a>
+          </div>
+          <div>
+            <h5>Need help?</h5>
+            <a href="https://status.configcat.com" target="_blank">Service Status Monitor</a>
+            <a href="https://join.slack.com/t/configcat-community/shared_invite/enQtMzkwNDg3MjQ3MTUyLWY1ZmE0NjY5NjA0YjAzMWU3MDg3ODhkMGI2ZjdmZjZmYTY4YTg1NmQ0YzBhMTVhOGE5YmNiYTI5OTg4NmNjYTQ" target="_blank">
+              Join our Community Slack
+            </a>
+            <a href="https://configcat.com/support" target="_blank">Contact us</a>
           </div>
         </section>
         <section className="copyright">{this.props.config.copyright}</section>
