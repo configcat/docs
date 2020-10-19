239208f4b80f3ab101a3d8d93259d4f2194cd0cf
sige <sige@configcat.com>
2020-10-19 16:44:48 +0200

    updating footer and header

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,27 +18,109 @@ module.exports = {
       items: [
         {
           href: 'https://www.configcat.com',
-          label: 'ConfigCat.com',
-          position: "right"
+          label: 'Home Page',
+          position: "left"
         },
         {
-            href: 'https://configcat.com/blog',
-            label: 'Blog',
-            position: "right"
+          href: 'https://www.configcat.com/pricing',
+          label: 'Pricing',
+          position: "left"
         },
         {
-            href: 'https://status.configcat.com',
-            label: 'Service Status Monitor',
-            position: "right"
-        }
+          href: 'https://www.configcat.com/architecture',
+          label: 'Architecture',
+          position: "left"
+        },
+        {
+          href: 'https://status.configcat.com',
+          label: 'Service Status Monitor',
+          position: "left"
+        },
+        {
+          href: 'https://configcat.com/blog',
+          label: 'Blog',
+          position: "left"
+        },
+
       ],
     },
     footer: {
       style: 'dark',
       links: [
         {
+          title: 'Legal & Company',
+          items: [
+            {
+              label: 'Terms & Policies',
+              href: 'https://configcat.com/policies',
+            },
+            {
+              label: 'Public Roadmap',
+              href: 'https://trello.com/b/8gYunIb6/configcat-public-roadmap',
+            },
+            {
+              label: 'Our Team',
+              href: 'https://configcat.com/team',
+            },
+            {
+              label: 'Jobs',
+              href: 'https://configcat.com/jobs',
+            },
+          ],
+        },
+        {
+          title: 'Know more',
+          items: [
+            {
+              label: 'Pricing',
+              href: 'https://configcat.com/pricing',
+            },
+            {
+              label: 'On-Premise Self Hosted Plan',
+              href: 'https://configcat.com/on-premise',
+            },
+            {
+              label: 'Referrals',
+              href: 'https://configcat.com/referrals',
+            },
+            {
+              label: 'Blog',
+              href: 'https://configcat.com/blog',
+            },
+          ],
+        },
+        {
+          title: 'Help & Resources',
+          items: [
+            {
+              label: 'Service Status Monitor',
+              href: 'https://status.configcat.com',
+            },
+            {
+              label: 'Architectural Overview',
+              href: 'https://configcat.com/architecture',
+            },
+            {
+              label: 'Public Management API Docs',
+              href: 'https://api.configcat.com/docs',
+            },
+            {
+              label: 'Support',
+              href: 'https://configcat.com/support',
+            },
+          ],
+        },
+        {
           title: 'Engage',
           items: [
+            {
+              label: 'Join our Community Slack',
+              href: 'https://join.slack.com/t/configcat-community/shared_invite/enQtMzkwNDg3MjQ3MTUyLWY1ZmE0NjY5NjA0YjAzMWU3MDg3ODhkMGI2ZjdmZjZmYTY4YTg1NmQ0YzBhMTVhOGE5YmNiYTI5OTg4NmNjYTQ',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/configcat',
+            },
             {
               label: 'Twitter',
               href: 'https://twitter.com/configcat',
@@ -53,40 +135,6 @@ module.exports = {
             },
           ],
         },
-        {
-          title: 'Know more',
-          items: [
-            {
-              label: 'ConfigCat.com',
-              href: 'https://configcat.com',
-            }, 
-            {
-              label: 'Blog',
-              href: 'https://configcat.com/blog',
-            }, 
-            {
-              label: 'GitHub repos',
-              href: 'https://github.com/configcat',
-            },
-          ],
-        },
-        {
-          title: 'Need help?',
-          items: [
-            {
-              label: 'Service Status Monitor',
-              href: 'https://status.configcat.com',
-            },
-            {
-              label: 'Join our Community Slack',
-              href: 'https://join.slack.com/t/configcat-community/shared_invite/enQtMzkwNDg3MjQ3MTUyLWY1ZmE0NjY5NjA0YjAzMWU3MDg3ODhkMGI2ZjdmZjZmYTY4YTg1NmQ0YzBhMTVhOGE5YmNiYTI5OTg4NmNjYTQ',
-            },
-            {
-              label: 'Contact us',
-              href: 'https://configcat.com/support',
-            },
-          ],
-        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} ConfigCat.`,
     },
@@ -97,7 +145,7 @@ module.exports = {
       apiKey: '36b9ea4801b9b88e1e8fa2e42d3cc705',
       indexName: 'configcat',
       searchParameters: {}
-  },
+    },
   },
   presets: [
     [
@@ -108,7 +156,7 @@ module.exports = {
           // Please change this to your repo.
           editUrl:
             'https://github.com/configcat/docs/edit/master/website/',
-            routeBasePath: '/'
+          routeBasePath: '/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
