0c27f36f6056b30ca5a9d1217f864ba78c0544f3
Clem Blanco <clement.blanco@gmail.com>
2023-02-21 18:54:43 +0100

    Create laravel.md for pod-point/laravel-configcat (#223)
    * Create laravel.md
    
    * Clarify Laravel configuration bits
    
    * adding laravel sdk ref to menu
    
    * adding laravel to main page
    
    * moving back elixir
    
    * reordering
    
    * typo
    
    * mennu item
    
    * rename
    
    * cleanup
    
    ---------
    
    Co-authored-by: sige <sige@configcat.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -150,6 +150,10 @@ module.exports = {
         { type: 'doc', id: 'integrations/amplitude', label: 'Amplitude' },
         { type: 'doc', id: 'integrations/vscode', label: 'Visual Studio Code' },
       ],
+      'Community Maintained': [
+        { type: 'doc', id: 'community-maintained/laravel', label: 'Package for Laravel' },
+        // { type: 'doc', id: 'community-maintained/deno', label: 'Deno' },
+      ],
       'Service Status': ['service/status'],
     },
     { type: 'doc', id: 'faq', label: 'FAQ' },
