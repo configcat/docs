5343d0c3d3d1eff1b68be23c28a17acee3394671
adams85 <31276480+adams85@users.noreply.github.com>
2025-08-11 19:43:26 +0200

    Unified JS SDK docs (#582)
    * Enable syntax highlighting, type checking and autocompletion for MDX files
    
    * Updates documentation for the new JS SDK
    
    * Add platform compatibility section
    
    * Add docs for query string flag overrides
    
    * Update HTTP proxy docs
    
    * Make JS-related SDK lists hierarchical everywhere
    
    * Update links to JS SDK docs
    
    * Update SDK version tables
    
    * Advertise simpler User Object creation syntax
    
    * Update CodePen demo
    
    * Add warning to hooks docs
    
    * Correct explanation of polling modes
    
    * Improve and make documentation of getAllKeys consistent
    
    * Add schema markups
    
    * Add migration guides
    
    * Correct .validationignore
    
    * Minor corrections
    
    * Improve JS SSR migration guide
    
    * Correct heading
    
    * Recommend Lazy Load polling mode in Cloudflare Worker SDK
    
    * Improve wording

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -75,13 +75,21 @@ const features = [
       { url: 'sdk-reference/elixir', title: 'Elixir' },
       { url: 'sdk-reference/go', title: 'Go' },
       { url: 'sdk-reference/java', title: 'Java' },
-      { url: 'sdk-reference/js', title: 'JavaScript' },
-      { url: 'sdk-reference/community/deno', title: 'JavaScript (Deno)' },
-      { url: 'sdk-reference/community/vue', title: 'JavaScript (Vue.js)' },
-      { url: 'sdk-reference/react', title: 'JavaScript (React)' },
-      { url: 'sdk-reference/js-ssr', title: 'JavaScript (SSR)' },
+      {
+        url: 'sdk-reference/js/overview',
+        title: 'JavaScript',
+        items: [
+          { url: 'sdk-reference/js/browser', title: 'Browser' },
+          { url: 'sdk-reference/js/bun', title: 'Bun' },
+          { url: 'sdk-reference/js/chromium-extension', title: 'Chromium Extension' },
+          { url: 'sdk-reference/js/cloudflare-worker', title: 'Cloudflare Worker' },
+          { url: 'sdk-reference/js/deno', title: 'Deno' },
+          { url: 'sdk-reference/js/node', title: 'Node.js' },
+          { url: 'sdk-reference/react', title: 'React' },
+          { url: 'sdk-reference/community/vue', title: 'Vue.js' },
+        ],
+      },
       { url: 'sdk-reference/kotlin', title: 'Kotlin Multiplatform' },
-      { url: 'sdk-reference/node', title: 'Node.js' },
       { url: 'sdk-reference/php', title: 'PHP' },
       { url: 'sdk-reference/community/laravel', title: 'PHP (Laravel)' },
       { url: 'sdk-reference/python', title: 'Python' },
@@ -135,14 +143,21 @@ function Feature({ imageUrl, title, description, links }) {
       )}
       <h3>{title}</h3>
       <p>{description}</p>
-      <ul>
-        {links.map((link, idx) => (
-          <li>
-            <Link to={useBaseUrl(link.url)}>{link.title}</Link>
-          </li>
-        ))}
-      </ul>
+      {FeatureItems(links)}
     </div>
+  );
+}
+
+function FeatureItems(links) {
+  return (
+    <ul class="feature-list">
+      {links.map(({ url, title, items }) => (
+        <li>
+          <Link to={useBaseUrl(url)}>{title}</Link>
+          {items?.length && FeatureItems(items)}
+        </li>
+      ))}
+    </ul>
   );
 }
 
