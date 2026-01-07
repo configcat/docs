cc3cc5c17915f3aa75fff3a366739361595b3432
Chavez Harris <74829200+codedbychavez@users.noreply.github.com>
2026-01-07 07:11:52 -0400

    Fix: Update search result links in compacted search box and search page (#641)
    * Update result link in compacted search
    
    * Update SearchBar
    
    * Fix links on search page
    
    * Refactor and format code

--- website/src/theme/SearchBar/index.tsx
+++ website/src/theme/SearchBar/index.tsx
@@ -5,20 +5,31 @@ import '@docsearch/css';
 export default function SearchBar() {
   return (
     <DocSearch
-      appId='0MLXBNIK0Q'
-      apiKey='6484bd6c163502bacf229cb8d22024ab'
+      appId="0MLXBNIK0Q"
+      apiKey="6484bd6c163502bacf229cb8d22024ab"
       indices={[
         {
           name: 'configcat', // Same as indexName in docusaurus.config.ts
         },
       ]}
-      resultsFooterComponent={({state}) => {
-          return (
-            <a href={`/docs/search?q=${encodeURIComponent(state.query)}`}>
-              See all results
-            </a>
-          );
-        }}
+      resultsFooterComponent={({ state }) => {
+        return (
+          <a href={`/docs/search?q=${encodeURIComponent(state.query)}`}>
+            See all results
+          </a>
+        );
+      }}
+      transformItems={(items) => {
+        const origin = window.location.origin;
+
+        return items.map((item) => {
+          const url = new URL(item.url);
+          return {
+            ...item,
+            url: `${origin}${url.pathname}${url.search}${url.hash}`,
+          };
+        });
+      }}
     />
   );
-};
+}
