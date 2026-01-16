bf17fb2a4ab81ed8e9ad913726e6db6562f2538a
Chavez Harris <codedbychavez@gmail.com>
2026-01-15 20:26:25 -0400

    Updates

--- website/src/theme/Heading/index.tsx
+++ website/src/theme/Heading/index.tsx
@@ -15,13 +15,10 @@ export default function Heading({as: As, id, ...props}: Props): ReactNode {
   // H1 headings do not need an id because they don't appear in the TOC.
   if (As === 'h1' || !id) {
     return (
-      <div className={'custom-header'}>
+      <div className="custom-h1-wrapper">
         <As {...props} id={undefined} />
-        {As === 'h1' && (
-          <CopyPageButton />
-        ) } 
+        {As === 'h1' && <CopyPageButton />} 
       </div>
-
     )
   }
 
