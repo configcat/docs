9d45e8151fed4c96bbe3461fafe5a06409d0bd0e
Lajos Szoke <63732287+laliconfigcat@users.noreply.github.com>
2026-01-22 11:49:38 +0100

    fix docsearch (#650)

--- website/src/theme/SearchBar/index.tsx
+++ website/src/theme/SearchBar/index.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { DocSearch } from '@docsearch/react';
+import { DocSearch, DocSearchProps } from '@docsearch/react';
 import '@docsearch/css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function SearchBar() {
+  const {siteConfig} = useDocusaurusContext();
   return (
     <DocSearch
-      appId="0MLXBNIK0Q"
-      apiKey="6484bd6c163502bacf229cb8d22024ab"
-      indices={[
-        {
-          name: 'docs', // Same as indexName in docusaurus.config.ts
-        },
-      ]}
+      {...(siteConfig.themeConfig.algolia as DocSearchProps)}
       resultsFooterComponent={({ state }) => {
         return (
           <a href={`/docs/search?q=${encodeURIComponent(state.query)}`}>
